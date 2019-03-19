8f078aba968fd5b2fc8671c71938d59e91cf988e
zmwilson <zmwilson91@gmail.com>
2019-03-18 22:49:36 -0700

    Started updating routes for HW11

--- server.py
+++ server.py
@@ -14,6 +14,10 @@ s3 = session.resource('s3')
 client = session.client('s3')
 bucket = s3.Bucket('zmwilson.cs493bucket')
 contents = {}
+genres = []
+artists = []
+albums = []
+songs = []
 
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin(supports_credentials=True)
@@ -29,6 +33,50 @@ def api_root():
 		)
 		contents[o.key] = url
 	resp = jsonify(contents)
+	resp.status_code = 200
+	return resp
+
+@app.route('/genres', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def genres():
+    genre1 = request.args.get('genre1')
+    genre2 = request.args.get('genre2')
+    genres.append(genre1)
+    genres.append(genre2)
+    resp = jsonify(genres)
+	resp.status_code = 200
+	return resp
+
+@app.route('/artists/for/genre', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def genres():
+    artist1 = request.args.get('Artist1')
+    artist2 = request.args.get('Artist2')
+    artists.append(artist1)
+    artists.append(artist2)
+    resp = jsonify(artists)
+	resp.status_code = 200
+	return resp
+
+@app.route('/albums/for/artist', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def genres():
+    album1 = request.args.get('album1')
+    album2 = request.args.get('album2')
+    albums.append(genre1)
+    albums.append(genre2)
+    resp = jsonify(albums)
+	resp.status_code = 200
+	return resp
+
+@app.route('/songs/for/album', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def genres():
+    song1 = request.args.get('song1')
+    song2 = request.args.get('song2')
+    songs.append(genre1)
+    songs.append(genre2)
+    resp = jsonify(songs)
 	resp.status_code = 200
 	return resp
 
